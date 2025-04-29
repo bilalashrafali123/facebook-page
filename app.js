d1997e36395800888a26584d7736754ec9d19de8
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-04-29 20:54:49 +0500

    updated

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 var username = document.querySelector("#userName")
 var passkey = document.querySelector("#password")
 
-
-
 function Login(){
 let uservalue = username.value
 let passvalue = passkey.value;
 
-
 if(uservalue == "" && passvalue == ""){
     alert("please enter useername and password first")
 }
-
 if(uservalue == "admin" && passvalue == "admin"){
     alert("login succesfully")
 }else{
     alert("login failed")
 }
-
-
-
-
-
 username.value = ""
 passkey.value = ""
 
