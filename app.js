0a0cb46cd16f4e08a3bfe37e4ef14ca037865987
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-04-29 18:16:45 +0500

    updated

---
+++ app.js
@@ -0,0 +1,19 @@
+var username = document.querySelector("#userName")
+var passkey = document.querySelector("#password")
+
+function Login(){
+let uservalue = username.value
+let passvalue = passkey.value;
+
+if(uservalue == "admin" && passvalue == "admin"){
+    alert("login succesfully")
+}else{
+    alert("login failed")
+}
+
+
+
+username.value = ""
+passkey.value = ""
+
+}
